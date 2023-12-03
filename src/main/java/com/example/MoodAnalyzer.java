d62182acb5573716acbc3b22d161c8d4adbcff79
Chinmay Bhalodia <94515096+chinmaybhalodia@users.noreply.github.com>
2023-12-03 15:40:13 +0530

    added custom exceptions for invalid mood messages

--- src/main/java/com/example/MoodAnalyzer.java
+++ src/main/java/com/example/MoodAnalyzer.java
@@ -10,18 +10,20 @@ public class MoodAnalyzer {
         this.message = message;
     }
 
-    public String analyzeMood() {
-        try {
-            // UC2: handle null message
-            if (this.message == null) {
-                throw new NullPointerException("Invlid Mood: message is null");
-            } else if (message.contains("Sad")) {
-                return "SAD";
-            } else {
-                return "HAPPY";
-            }
-        } catch (NullPointerException exception) {
-            return "HAPPY"; // return happy if null
+    public String analyzeMood() throws MoodAnalysisException {
+        // UC2: handle null message
+        if (this.message == null) {
+            throw new MoodAnalysisException(MoodErrorType.NULL, "Invalid input: message is null");
+        } 
+        
+        if (this.message.isEmpty()) {
+            throw new MoodAnalysisException(MoodErrorType.EMPTY, "Invalid input: message is empty");
+        }
+        
+        if (this.message.contains("Sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
         }
     }
 }
